39fda7c213ed88c8884c34b6f485badd15020f79
Sofia Gutierrez <sofia.mgutie@gmail.com>
2020-10-29 22:46:31 -0600

    modified switches, added song method in buzzer

--- project/src/buzzer.c
+++ project/src/buzzer.c
@@ -2,6 +2,7 @@
 #include "libTimer.h"
 #include "buzzer.h"
 
+
 void buzzer_init()
 {
     /* 
@@ -24,8 +25,12 @@ void buzzer_set_period(short cycles) /* buzzer clock = 2MHz.  (period of 1k resu
   CCR1 = cycles >> 1;		/* one half cycle */
 }
 
-
-    
-    
-  
-
+void song()
+{
+  int song[1] ={1000};
+  int i = 0;
+  while(i){
+    buzzer_set_period(song[i]);
+    i++;
+  }
+}
--- project/src/switches.c
+++ project/src/switches.c
@@ -3,24 +3,25 @@
 #include "led.h"
 
 char switch_state_down, switch_state_changed; /* effectively boolean */
+char S1, S2, S3, S4;
 
 static char 
 switch_update_interrupt_sense()
 {
-  char p1val = P1IN;
+  char p2val = P2IN;
   /* update switch interrupt to detect changes from current buttons */
-  P1IES |= (p1val & SWITCHES);	/* if switch up, sense down */
-  P1IES &= (p1val | ~SWITCHES);	/* if switch down, sense up */
-  return p1val;
+  P2IES |= (p2val & SWITCHES);	/* if switch up, sense down */
+  P2IES &= (p2val | ~SWITCHES);	/* if switch down, sense up */
+  return p2val;
 }
 
 void 
 switch_init()			/* setup switch */
 {  
-  P1REN |= SWITCHES;		/* enables resistors for switches */
-  P1IE |= SWITCHES;		/* enable interrupts from switches */
-  P1OUT |= SWITCHES;		/* pull-ups for switches */
-  P1DIR &= ~SWITCHES;		/* set switches' bits for input */
+  P2REN |= SWITCHES;		/* enables resistors for switches */
+  P2IE |= SWITCHES;		/* enable interrupts from switches */
+  P2OUT |= SWITCHES;		/* pull-ups for switches */
+  P2DIR &= ~SWITCHES;		/* set switches' bits for input */
   switch_update_interrupt_sense();
   led_update();
 }
@@ -28,8 +29,29 @@ switch_init()			/* setup switch */
 void
 switch_interrupt_handler()
 {
-  char p1val = switch_update_interrupt_sense();
-  switch_state_down = (p1val & SW1) ? 0 : 1; /* 0 when SW1 is up */
+  char p2val = switch_update_interrupt_sense();
+  S1 = (p2val & SW1) ? 0 : 1; /* 0 when SW1 is up */
+  S2 = (p2val & SW2) ? 0 : 1;
+  S3 = (p2val & SW3) ? 0 : 1;
+  S4 = (p2val & SW4) ? 0 : 1;
+  if(S1){
+    song();
+  }
+  if(S2){
+    //buzzer_set_period(1050);
+    //buzzer_set_period(2100);
+    buzzer_set_period(3150);
+  }
+  if(S3){
+    //buzzer_set_period(920);
+    //buzzer_set_period(1840);
+    buzzer_set_period(2760);
+  }
+  if(S4){
+    //buzzer_set_period(780);
+    //buzzer_set_period(1560);
+    buzzer_set_period(2340);
+  }
   switch_state_changed = 1;
   led_update();
 }
